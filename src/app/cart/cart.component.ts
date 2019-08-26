c7fe8a2d2d524fad685892981dc15039170ab5e0
RAllner <raphaelallner@yahoo.de>
2019-08-26 12:17:35 +0200

    3 files modified, 1 added to get the cart view work

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -12,9 +12,14 @@ export class CartComponent implements OnInit {
   items = []
   constructor(
     private cartService: CartService
-    ) { }
+    ) {
+          this.items = this.cartService.getItems();
+     }
+
+  
 
   ngOnInit() {
+
   }
 
 }
